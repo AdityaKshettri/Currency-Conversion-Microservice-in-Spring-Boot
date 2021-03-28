f54dd59466b4901adc6f468b7f858e5fe96aa578
AdityaKshettri <adikshettri1623@gmail.com>
2021-03-28 20:45:12 +0530

    docker + zipkin config

--- currency-exchange-service/src/main/java/com/aditya/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/aditya/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,11 +1,13 @@
 package com.aditya.microservices.currencyexchangeservice;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CurrencyExchangeController {
@@ -16,6 +18,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        log.info("retrieveExchangeValue() called with {} to {}", from, to);
         CurrencyExchange currencyExchange = currencyExchangeRepository.findByFromAndTo(from, to);
         if(currencyExchange == null) {
             throw new RuntimeException("Unable to find data for " + from + " to " + to);
